8b90668e13e8661237a78cf9e2603477096143f2
BeFreeCODE <kihanlll@gmail.com>
2017-06-13 19:22:38 +0900

    test

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+
+    //충돌
+    private void OnTriggerEnter(Collider other)
+    {
+        //방향전환 아이템.
+        if (other.transform.tag.Equals("DirItem"))
+        {
+            int dirNum = other.GetComponent<DirItem>().DIRNUM;
+            SetPlayerDirection(dirNum);
+
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    //플레이어 방향 전환
+    private void SetPlayerDirection(int _dirNum)
+    {
+        Vector3 newDirection = Vector3.zero;
+
+        switch (_dirNum)
+        {
+            case 1:
+                newDirection = this.transform.position + Vector3.up;
+                break;
+            case 2:
+                newDirection = this.transform.position + Vector3.up + Vector3.left;
+                break;
+            case 3:
+                newDirection = this.transform.position + Vector3.left;
+                break;
+            case 4:
+                newDirection = this.transform.position + Vector3.left + Vector3.down;
+                break;
+            case 5:
+                newDirection = this.transform.position + Vector3.down;
+                break;
+            case 6:
+                newDirection = this.transform.position + Vector3.down + Vector3.right;
+                break;
+            case 7:
+                newDirection = this.transform.position + Vector3.right;
+                break;
+            case 8:
+                newDirection = this.transform.position + Vector3.up + Vector3.right;
+                break;
+        }
+
+        if (newDirection != Vector3.zero)
+        {
+            this.transform.LookAt(newDirection);
+        }
+    }
+}
---
+++ Assets/Script/TouchManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    private Vector2 startVec;
+    private Vector2 dragVec;
+    private Vector2 moveVec;
+
+    public bool isDrag;
+
+    private void OnMouseDown()
+    {
+        //마우스 위치 변수저장
+        startVec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        dragVec = startVec;
+
+        //마우스 위치와 현재 플레이어 위치 차이 값
+        moveVec = new Vector2(startVec.x - player.transform.position.x,
+                                startVec.y - player.transform.position.y);
+    }
+
+    private void OnMouseDrag()
+    {
+        if (startVec != dragVec)
+        {
+            //Drag 시작
+            isDrag = true;
+        }
+
+        //드래그 위치 업데이트
+        dragVec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (isDrag)
+        {
+            //moveVec 차이값만큼 간격을 두고 이동.
+            //Mathf.Clamp(min, max 지정)
+            player.transform.position = new Vector3(Mathf.Clamp(dragVec.x - moveVec.x, -5.2f, 5.2f),
+                                                     Mathf.Clamp(dragVec.y - moveVec.y, -9.2f, 9.2f),
+                                                     0f);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        //드래그 종료
+        isDrag = false;
+    }
+}
